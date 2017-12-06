9e1d10a3505243a6bd39f92bf019b89017838fb2
Gregory Amerson <gregory.amerson@liferay.com>
2017-12-05 18:08:00 -0600

    LPS-76066 rename package

--- modules/sdk/project-templates/project-templates-test/src/testIntegration/java/com/liferay/project/templates/test/ProjectTemplatesIntegrationTest.java
+++ modules/sdk/project-templates/project-templates-test/src/testIntegration/java/com/liferay/project/templates/test/ProjectTemplatesIntegrationTest.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.project.templates.integration.test;
+package com.liferay.project.templates.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.portal.kernel.util.PropsKeys;
