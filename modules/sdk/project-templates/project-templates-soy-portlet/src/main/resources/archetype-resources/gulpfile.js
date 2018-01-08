54c64c3d8ec83422d6c7b52a10d0fdc50cc34295
Bruno Basto <bruno.basto@liferay.com>
2018-01-08 15:00:03 -0300

    LPS-76201 SF

--- modules/sdk/project-templates/project-templates-soy-portlet/src/main/resources/archetype-resources/gulpfile.js
+++ modules/sdk/project-templates/project-templates-soy-portlet/src/main/resources/archetype-resources/gulpfile.js
@@ -1,53 +1,55 @@
-var gulp = require('gulp');
-var exec = require('child_process').exec;
+const { fork } = require('child_process');
+const gulp = require('gulp');
+
+const resourcesDir = `${__dirname}/target/classes/META-INF/resources`;
 
 const defaults = {
-		  encoding: 'utf8',
-		  timeout: 0,
-		  maxBuffer: 200 * 1024,
-		  killSignal: 'SIGTERM',
-		  cwd: __dirname + '/target/classes/META-INF/resources',
-		  env: null
-		};
+	cwd: resourcesDir,
+	encoding: 'utf8',
+	env: null,
+	killSignal: 'SIGTERM',
+	maxBuffer: 200 * 1024,
+	stdio: 'inherit',
+	timeout: 0
+};
 
-gulp.task('config-js-modules', function(cb) {
+function callNode(args, callback) {
+	process.chdir(resourcesDir);
+	fork(`${__dirname}/target/node/node`, args, defaults)
+	.on('close', callback);
+}
 
-	process.chdir(__dirname + '/target/classes/META-INF/resources');
-	exec(__dirname + '/target/node/node ' + __dirname + '/node_modules/liferay-module-config-generator/bin/index.js ' +
-		  '--config= ' +
-		  '--extension= ' +
-		  '--filePattern **/resources/*.js ' +
-		  '--format /_/g,- ' +
-		  '--ignorePath true ' +
-		  '--moduleConfig ' + __dirname + '/package.json ' +
-		  '--namespace Liferay.Loader ' +
-		  '--output ' + __dirname + '/target/classes/META-INF/config.json ' +
-		  '--moduleRoot ' + __dirname + '/target/classes/META-INF/resources ' +
-		  __dirname + '/target/classes/META-INF', function(err, stdout, stderr) {
-	console.log(stdout);
-	console.log(stderr);
-	cb(err);
-}, defaults);
-})
+gulp.task('config-js-modules', function(done) {
+	callNode([
+		 `${__dirname}/node_modules/liferay-module-config-generator/bin/index.js`,
+		 '--config=',
+		 '--extension=',
+		 '--filePattern **/resources/*.js',
+		 '--format /_/g,-',
+		 '--ignorePath true',
+		 `--moduleConfig ${__dirname}/package.json`,
+		 '--namespace Liferay.Loader',
+		 `--output ${__dirname}/target/classes/META-INF/config.json`,
+		 `--moduleRoot resourcesDir`,
+		 `${__dirname}/target/classes/META-INF`
+	], done);
+});
 
-gulp.task('transpile-js', function(cb) {
-
-	process.chdir(__dirname + '/target/classes/META-INF/resources');
-	exec(__dirname + '/target/node/node ' + __dirname + '/node_modules/metal-cli/index.js build ' +
-		  '--bundleFileName= ' +
-		  '--dest ' + __dirname + '/target/classes/META-INF/resources ' +
-		  '--format amd ' +
-		  '--globalName= ' +
-		  '--moduleName= ' +
-		  '--soyDeps ' +
-		  __dirname + '/node_modules/lexicon*/src/**/*.soy ' +
-		  __dirname + '/node_modules/metal*/src/**/*.soy ' +
-		  '--soyDest ' + __dirname + '/target/classes/META-INF/resources ' +
-		  '--soySrc **/*.soy ' +
-		  '--src **/*.es.js ' +
-		  '**/*.soy.js', function(err, stdout, stderr) {
-	console.log(stdout);
-	console.log(stderr);
-	cb(err);
-}, defaults);
-})
+gulp.task('transpile-js', function(done) {
+	callNode([
+		`${__dirname}/node_modules/metal-cli/index.js`,
+		'build',
+		'--bundleFileName=',
+		`--dest ${resourcesDir}`,
+		'--format amd',
+		'--globalName=',
+		'--moduleName=',
+		'--soyDeps',
+		`${__dirname}/node_modules/lexicon*/src/**/*.soy`,
+		`${__dirname}/node_modules/metal*/src/**/*.soy`,
+		`--soyDest  ${resourcesDir}`,
+		'--soySrc **/*.soy',
+		'--src **/*.es.js ',
+		'**/*.soy.js'
+	], done);
+});
