754ea50cfb68dfc1b1c3a23ce8f01b46884a0306
Bruno Basto <bruno.basto@liferay.com>
2018-01-09 11:26:27 -0300

    LPS-76201 Fixes call to child_process.fork

--- modules/sdk/project-templates/project-templates-soy-portlet/src/main/resources/archetype-resources/gulpfile.js
+++ modules/sdk/project-templates/project-templates-soy-portlet/src/main/resources/archetype-resources/gulpfile.js
@@ -14,23 +14,30 @@ const defaults = {
 };
 
 function callNode(args, callback) {
-	process.chdir(resourcesDir);
-	fork(`${__dirname}/target/node/node`, args, defaults)
+	process.chdir(resourcesDir); 
+	fork(args.shift(), args, defaults)
 	.on('close', callback);
 }
 
 gulp.task('config-js-modules', function(done) {
 	callNode([
-		 `${__dirname}/node_modules/liferay-module-config-generator/bin/index.js`,
-		 '--config=',
-		 '--extension=',
-		 '--filePattern **/resources/*.js',
-		 '--format /_/g,-',
-		 '--ignorePath true',
-		 `--moduleConfig ${__dirname}/package.json`,
-		 '--namespace Liferay.Loader',
-		 `--output ${__dirname}/target/classes/META-INF/config.json`,
-		 `--moduleRoot resourcesDir`,
+		`${__dirname}/node_modules/liferay-module-config-generator/bin/index.js`,
+		 '--config',
+		 '--extension',
+		 '--filePattern',
+		 '**/resources/*.js',
+		 '--format',
+		 '/_/g,-',
+		 '--ignorePath',
+		 'true',
+		 '--moduleConfig',
+		 `${__dirname}/package.json`,
+		 '--namespace',
+		 'Liferay.Loader',
+		 '--output',
+		 `${__dirname}/target/classes/META-INF/config.json`,
+		 '--moduleRoot',
+		 resourcesDir,
 		 `${__dirname}/target/classes/META-INF`
 	], done);
 });
@@ -39,17 +46,22 @@ gulp.task('transpile-js', function(done) {
 	callNode([
 		`${__dirname}/node_modules/metal-cli/index.js`,
 		'build',
-		'--bundleFileName=',
-		`--dest ${resourcesDir}`,
-		'--format amd',
-		'--globalName=',
-		'--moduleName=',
+		'--bundleFileName',
+		`--dest`,
+		resourcesDir,
+		'--format',
+		'amd',
+		'--globalName',
+		'--moduleName',
 		'--soyDeps',
 		`${__dirname}/node_modules/lexicon*/src/**/*.soy`,
 		`${__dirname}/node_modules/metal*/src/**/*.soy`,
-		`--soyDest  ${resourcesDir}`,
-		'--soySrc **/*.soy',
-		'--src **/*.es.js ',
+		`--soyDest`,
+		resourcesDir,
+		'--soySrc',
+		'**/*.soy',
+		'--src',
+		'**/*.es.js',
 		'**/*.soy.js'
 	], done);
 });
