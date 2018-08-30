e4910e5fc5757eb8ec1de73b73775c3e66022f6a
Gregory Amerson <gregory.amerson@liferay.com>
2018-08-29 21:27:04 -0500

    LPS-84857 consistency

--- modules/sdk/project-templates/project-templates/src/main/java/com/liferay/project/templates/internal/ArchetyperArchetypeArtifactManager.java
+++ modules/sdk/project-templates/project-templates/src/main/java/com/liferay/project/templates/internal/ArchetyperArchetypeArtifactManager.java
@@ -38,9 +38,17 @@ import org.apache.maven.artifact.repository.ArtifactRepository;
 public class ArchetyperArchetypeArtifactManager
 	extends DefaultArchetypeArtifactManager {
 
+<<<<<<< HEAD
 	public ArchetyperArchetypeArtifactManager(List<File> archetypesFiles) {
 		_archetypesFiles = archetypesFiles;
 
+=======
+	public ArchetyperArchetypeArtifactManager(List<File> archetypesFiles)
+		throws Exception {
+
+		_archetypesFiles = archetypesFiles;
+
+>>>>>>> LPS-84857 consistency
 		if (_archetypesFiles.isEmpty()) {
 			try {
 				_archetypesFiles.add(
@@ -118,6 +126,9 @@ public class ArchetyperArchetypeArtifactManager
 	}
 
 	private final List<File> _archetypesFiles;
+<<<<<<< HEAD
 
+=======
+>>>>>>> LPS-84857 consistency
 
 }
