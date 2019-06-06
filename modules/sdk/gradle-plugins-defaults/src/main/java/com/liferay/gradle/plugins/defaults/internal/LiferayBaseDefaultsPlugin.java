1c573162ec3973e84d54f781858012131e734170
Gregory Amerson <gregory.amerson@liferay.com>
2019-06-05 22:59:44 -0500

    LPS-96590 setBuildProfile initial prototype

--- modules/sdk/gradle-plugins-defaults/src/main/java/com/liferay/gradle/plugins/defaults/internal/LiferayBaseDefaultsPlugin.java
+++ modules/sdk/gradle-plugins-defaults/src/main/java/com/liferay/gradle/plugins/defaults/internal/LiferayBaseDefaultsPlugin.java
@@ -8,6 +8,8 @@ package com.liferay.gradle.plugins.defaults.internal;
 import com.liferay.gradle.plugins.BaseDefaultsPlugin;
 import com.liferay.gradle.plugins.LiferayBasePlugin;
 import com.liferay.gradle.plugins.defaults.internal.util.GradleUtil;
+import com.liferay.gradle.plugins.defaults.tasks.CleanBuildProfileTask;
+import com.liferay.gradle.plugins.defaults.tasks.SetBuildProfileTask;
 import com.liferay.gradle.plugins.extensions.LiferayExtension;
 
 import java.io.File;
@@ -15,9 +17,11 @@ import java.io.File;
 import org.gradle.api.Action;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.Task;
 
 /**
  * @author Andrea Di Giorgi
+ * @author Gregory Amerson
  */
 public class LiferayBaseDefaultsPlugin
 	extends BaseDefaultsPlugin<LiferayBasePlugin> {
@@ -29,12 +33,18 @@ public class LiferayBaseDefaultsPlugin
 	protected void applyPluginDefaults(
 		Project project, LiferayBasePlugin liferayBasePlugin) {
 
+		GradleUtil.addTask(
+			project, "cleanBuildProfile", CleanBuildProfileTask.class);
+		GradleUtil.addTask(
+			project, "setBuildProfile", SetBuildProfileTask.class);
+
 		project.afterEvaluate(
 			new Action<Project>() {
 
 				@Override
 				public void execute(Project project) {
 					_configureLiferayDeployDir(project);
+					_configureBuildProfileTasks(project);
 				}
 
 			});
@@ -46,6 +56,14 @@ public class LiferayBaseDefaultsPlugin
 	}
 
 	private LiferayBaseDefaultsPlugin() {
+	}
+
+	private void _configureBuildProfileTasks(Project project) {
+		Task cleanBuildProfile = GradleUtil.getTask(
+			project, "cleanBuildProfile");
+		Task setBuildProfile = GradleUtil.getTask(project, "setBuildProfile");
+
+		setBuildProfile.dependsOn(cleanBuildProfile);
 	}
 
 	private void _configureLiferayDeployDir(Project project) {
