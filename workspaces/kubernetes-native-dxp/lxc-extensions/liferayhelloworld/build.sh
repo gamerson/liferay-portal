9de990f2e6f72b5fbfcf9d7d0d137751df5cc613
Gregory Amerson <gregory.amerson@liferay.com>
2022-04-25 09:52:49 -0500

    LCD-14083 helm WIP

--- workspaces/kubernetes-native-dxp/lxc-extensions/liferayhelloworld/build.sh
+++ workspaces/kubernetes-native-dxp/lxc-extensions/liferayhelloworld/build.sh
@@ -8,10 +8,3 @@ echo "[run_local] Build the liferayhelloworld PoC"
 
 yarn install && yarn build-local &&\
   docker build -t $IMAGE .
-
-cat ../../k8s/liferayhelloworld/extension-configmap.yaml.template \
-	> ../../k8s/liferayhelloworld/extension-configmap.yaml &&
-sed -e 's/^/    /' com.liferay.remote.app.factory.configuration.v1.RemoteAppFactoryConfiguration-liferayhelloworld.config \
-	>> ../../k8s/liferayhelloworld/extension-configmap.yaml
-
-
