5bd7bcdf576ba7054610906e8db8f1879d77a4a5
Raymond Augé <raymond.auge@liferay.com>
2022-05-24 11:06:31 -0400

    LPS-151349 cleanup

--- workspaces/kubernetes-native-dxp/init_cluster.sh
+++ workspaces/kubernetes-native-dxp/init_cluster.sh
@@ -50,9 +50,7 @@ else
     exit 1
 fi
 
-# if [ `which skaffold` ]; then
-#     skaffold dev
-# else
-#     echo "Please install skaffold. See https://skaffold.dev/docs/install/"
-#     exit 1
-# fi
+(cd lxc-extensions/customrestservice/ && rm -rf build && IMAGE=customrestservice ./build.sh)
+(cd lxc-extensions/liferayhelloworld/ && rm -rf build && IMAGE=liferayhelloworld ./build.sh)
+(cd lxc-extensions/dynamicjscss/ && rm -rf build && IMAGE=dynamicjscss ./build.sh)
+(cd lxc-extensions/designpackfire/ && rm -rf build && IMAGE=designpackfire ./build.sh)
