5b5b270ca3cb5e133d6fcd7f269454ec05056f34
Gregory Amerson <gregory.amerson@liferay.com>
2022-08-01 15:38:17 -0500

    use self signed certificate for tls

--- workspaces/service-extensions-workspace/k8s/start-k3d.sh
+++ workspaces/service-extensions-workspace/k8s/start-k3d.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
-k3d cluster create --config k3d/config.yaml --registry-create registry.localdev.me:5000
+k3d cluster create \
+  --config k3d/config.yaml \
+  --registry-create registry.localdev.me:5000
 
-kubectl create -f k3d/rbac.yaml
+kubectl create \
+  -f k3d/rbac.yaml
+
+kubectl create secret generic localdev-tls-secret \
+  --from-file=tls.crt=./tls/localdev.me.crt \
+  --from-file=tls.key=./tls/localdev.me.key  \
+  --namespace kube-system
