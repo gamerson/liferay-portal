5abb39b1b7dc970f05826b29aa8ba1ea1f71b3f6
Gregory Amerson <gregory.amerson@liferay.com>
2022-08-02 18:07:56 -0500

    remove kind scripts and modify dxp metadata configmaps

--- workspaces/service-extensions-workspace/k8s/cluster-start.sh
+++ workspaces/service-extensions-workspace/k8s/cluster-start.sh
@@ -2,10 +2,8 @@
 
 k3d cluster create \
   --config k3d/config.yaml \
-  --registry-create registry.localdev.me:5000
-
-kubectl create \
-  -f k3d/rbac.yaml
+  --registry-create registry.localdev.me:5000 \
+  --wait
 
 kubectl create secret generic localdev-tls-secret \
   --from-file=tls.crt=./tls/localdev.me.crt \
