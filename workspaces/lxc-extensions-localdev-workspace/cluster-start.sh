14edde4838627819c2cfabe5e7f6db5a09e2e907
Gregory Amerson <gregory.amerson@liferay.com>
2022-08-08 14:09:46 -0500

    lxc-extensions-localdev-workspace

---
+++ workspaces/lxc-extensions-localdev-workspace/cluster-start.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+
+k3d cluster create \
+  --config ./k8s/k3d/config.yaml \
+  --registry-create registry.localdev.me:5000 \
+  --wait
+
+kubectl config use-context k3d-lxc-localdev
+kubectl config set-context --current --namespace=default
+
+kubectl create secret generic localdev-tls-secret \
+  --from-file=tls.crt=./k8s/tls/localdev.me.crt \
+  --from-file=tls.key=./k8s/tls/localdev.me.key  \
+  --namespace default
+
+echo "Cluster is ready.  Run 'tilt up' to deploy DXP and extensions"
