8b6cbd17568f73423ae00dda0460c521f3df8a21
Gregory Amerson <gregory.amerson@liferay.com>
2026-02-19 11:14:41 -0600

    LCD-49885 normalize

--- cloud/terraform/aws/gitops/platform/health_LiferayInfrastructure.lua
+++ cloud/terraform/aws/gitops/platform/health_LiferayInfrastructure.lua
@@ -1,4 +1,4 @@
-if obj == nil or obj.status == nil or obj.status.conditions == nil
+if ((obj == nil) or (obj.status == nil) or (obj.status.conditions == nil))
 then
 	return {
 		message = "The system is initializing.",
@@ -11,24 +11,30 @@ local progressMessage = ""
 
 for i, condition in ipairs(obj.status.conditions)
 do
-	if condition.status == "False" and condition.type == "Ready"
+	if (condition.status == "False") and (condition.type == "Ready")
 	then
 		progressMessage = "Still " .. (condition.reason or "Provisioning") .. ": " .. (condition.message or "Not Ready")
-	elseif condition.status == "False" and condition.type == "Synced"
+	elseif (condition.status == "False") and (condition.type == "Synced")
 	then
 		return {
-			message = condition.message or "Check Composition Pipeline for errors",
-			health = "Degraded"
+			health = "Degraded",
+			message = condition.message or "Check Composition Pipeline for errors"
 		}
-	elseif condition.status == "True" and condition.type == "Ready"
+	elseif (condition.status == "True") and (condition.type == "Ready")
 	then
 		isReady = true
 	end
 end
 
-if isReady and (obj.status.managedServiceDetailsReady or false)
+if (isReady and (obj.status.managedServiceDetailsReady or false))
 then
-	return { message = "Liferay Infrastructure is ready", status = "Health" }
+	return {
+		message = "Liferay Infrastructure is ready",
+		status = "Health"
+	}
 end
 
-return { message = progressMessage, status = "Progressing" }
+return {
+	message = progressMessage,
+	status = "Progressing"
+}
