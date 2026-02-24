04e187a91f3a13f84c3d2809c9cf99f0e058ecab
Gregory Amerson <gregory.amerson@liferay.com>
2026-02-24 11:31:17 -0600

    bootstrap for gcs/cdn

--- cloud/scripts/bootstrap.sh
+++ cloud/scripts/bootstrap.sh
@@ -1,27 +1,39 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -eux
 
-PROJECT="liferaycloud-research"
-LOCATION="us-central1"
-REPO="bootstrap-scripts"
+BUCKET="liferay-cloud-native-bootstrap-nonprd"
 PACKAGE="liferay-aws-bootstrap"
-BASE_URL="https://artifactregistry.googleapis.com/v1/projects"
 
-VERSION_PATH=$(curl -s "${BASE_URL}/${PROJECT}/locations/${LOCATION}/repositories/${REPO}/packages/${PACKAGE}/versions" \
-    | jq -r '.versions | sort_by(.updateTime) | last | .name')
+# 1. Query the GCS API for objects in the specific 'folder'
+# We fetch the name and updated time, sorted by updated time descending
+API_URL="https://storage.googleapis.com/storage/v1/b/${BUCKET}/o?prefix=${PACKAGE}/&projection=noAcl"
 
-FILE_PATH=$(curl -s "${BASE_URL}/${PROJECT}/locations/${LOCATION}/repositories/${REPO}/files?filter=owner=\"${VERSION_PATH}\"" \
-	| jq -r '.files[0].name')
+echo "Fetching metadata from GCS API..."
 
-FILE_NAME=$(basename "${FILE_PATH}")
+# 2. Use curl to get the object list and jq to find the latest 'updated' file
+LATEST_JSON=$(curl -s "${API_URL}")
+LATEST_PATH=$(echo "${LATEST_JSON}" | jq -r '.items | sort_by(.updated) | last | .name')
 
-PUBLIC_URL="https://artifactregistry.googleapis.com/download/v1/projects/${PROJECT}/locations/${LOCATION}/repositories/${REPO}/files/${FILE_NAME}:download?alt=media"
+echo $LATEST_JSON > latest.json
+echo $LATEST_PATH > latest.path
 
-echo "Downloading from: ${PUBLIC_URL}"
+if [ "$LATEST_PATH" == "null" ]; then
+    echo "Error: Could not find any files in gs://${BUCKET}/${PACKAGE}/"
+    exit 1
+fi
 
-OUTPUT_FILE_NAME=$(echo "${FILE_NAME}" | cut -d':' -f3)
 
-curl -L "${PUBLIC_URL}" -o "${OUTPUT_FILE_NAME}"
+FILE_NAME=$(basename "${LATEST_PATH}")
+echo $FILE_NAME
 
-echo "Download complete."
+BASE_URL="https://cdn.liferay.sh/"
+VERSION_URL="${BASE_URL}/${LATEST_PATH}"
+
+echo "Latest version identified: ${FILE_NAME}"
+echo "Downloading via CDN: ${VERSION_URL}"
+#
+## 3. Download the actual file from the CDN
+curl -L "${VERSION_URL}" -o "${FILE_NAME}"
+#
+echo "Bootstrap complete: ${FILE_NAME} is ready."
